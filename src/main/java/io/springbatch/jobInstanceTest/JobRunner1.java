6725e89fb7db04b62a33b11d25f48b67cc3bcfd9
koiw1202 <koiw1202@naver.com>
2025-01-01 18:01:12 +0900

    Job Paramters 공부

--- src/main/java/io/springbatch/jobInstanceTest/JobRunner1.java
+++ src/main/java/io/springbatch/jobInstanceTest/JobRunner1.java
@@ -1,37 +1,37 @@
-package io.springbatch.jobInstanceTest;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-01-01        koiw1       최초 생성
- */
-@Component
-public class JobRunner1 implements ApplicationRunner {
-
-    @Autowired
-    JobLauncher jobLauncher;
-
-    @Autowired
-    Job job;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2").toJobParameters();
-
-        jobLauncher.run(job, jobParameters);
-
-    }
-}
+//package io.springbatch.jobInstanceTest;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersBuilder;
+//import org.springframework.batch.core.launch.JobLauncher;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2025-01-01        koiw1       최초 생성
+// */
+//@Component
+//public class JobRunner1 implements ApplicationRunner {
+//
+//    @Autowired
+//    JobLauncher jobLauncher;
+//
+//    @Autowired
+//    Job job;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//        JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2").toJobParameters();
+//
+//        jobLauncher.run(job, jobParameters);
+//
+//    }
+//}
