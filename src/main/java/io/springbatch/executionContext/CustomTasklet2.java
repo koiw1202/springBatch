d4ad185c984cf67401466a5add8cdece6075b4ca
koiw1202 <koiw1202@naver.com>
2025-01-12 14:01:12 +0900

    JobLauncher 공부

--- src/main/java/io/springbatch/executionContext/CustomTasklet2.java
+++ src/main/java/io/springbatch/executionContext/CustomTasklet2.java
@@ -1,24 +1,24 @@
-package io.springbatch.executionContext;
-
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-01-05        koiw1       최초 생성
- */
-public class CustomTasklet2 implements Tasklet {
-
-    @Override
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-
-        System.out.println("step2 executed");
-
-        return RepeatStatus.FINISHED;
-    }
-}
+//package io.springbatch.executionContext;
+//
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2025-01-05        koiw1       최초 생성
+// */
+//public class CustomTasklet2 implements Tasklet {
+//
+//    @Override
+//    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//
+//        System.out.println("step2 executed");
+//
+//        return RepeatStatus.FINISHED;
+//    }
+//}
--- src/main/java/io/springbatch/jobRepository/JobRepositoryListner.java
+++ src/main/java/io/springbatch/jobRepository/JobRepositoryListner.java
@@ -1,59 +1,59 @@
-package io.springbatch.jobRepository;
-
-import org.springframework.batch.core.*;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JobRepositoryListner implements JobExecutionListener {
-
-    @Autowired
-    private JobRepository jobRepository;
-
-    @Override
-    public void beforeJob(JobExecution jobExecution) {
-
-    }
-
-    @Override
-    public void afterJob(JobExecution jobExecution) {
-        String jobName=jobExecution.getJobInstance().getJobName();
-
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("requestDate", "20240101").toJobParameters();
-
-        JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
-
-        if(lastJobExecution != null) {
-            for(StepExecution stepExecution : lastJobExecution.getStepExecutions()) {
-
-                BatchStatus batchStatus = stepExecution.getStatus();
-                System.out.println("Status : " + batchStatus);
-
-                ExitStatus exitStatus = stepExecution.getExitStatus();
-                System.out.println("exisStatus : " + exitStatus);
-
-                String stepName = stepExecution.getStepName();
-                System.out.println("stempName : " + stepName);
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package io.springbatch.jobRepository;
+//
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class JobRepositoryListner implements JobExecutionListener {
+//
+//    @Autowired
+//    private JobRepository jobRepository;
+//
+//    @Override
+//    public void beforeJob(JobExecution jobExecution) {
+//
+//    }
+//
+//    @Override
+//    public void afterJob(JobExecution jobExecution) {
+//        String jobName=jobExecution.getJobInstance().getJobName();
+//
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("requestDate", "20240101").toJobParameters();
+//
+//        JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
+//
+//        if(lastJobExecution != null) {
+//            for(StepExecution stepExecution : lastJobExecution.getStepExecutions()) {
+//
+//                BatchStatus batchStatus = stepExecution.getStatus();
+//                System.out.println("Status : " + batchStatus);
+//
+//                ExitStatus exitStatus = stepExecution.getExitStatus();
+//                System.out.println("exisStatus : " + exitStatus);
+//
+//                String stepName = stepExecution.getStepName();
+//                System.out.println("stempName : " + stepName);
+//            }
+//        }
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
