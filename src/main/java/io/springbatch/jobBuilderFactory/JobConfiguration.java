b3ded55050fbc5868659cab2436959e225dae760
koiw1202 <koiw1202@naver.com>
2025-01-16 23:17:27 +0900

    SimpleJob 공부

--- src/main/java/io/springbatch/jobBuilderFactory/JobConfiguration.java
+++ src/main/java/io/springbatch/jobBuilderFactory/JobConfiguration.java
@@ -1,143 +1,143 @@
-package io.springbatch.jobBuilderFactory;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-01-01        koiw1       최초 생성
- */
-@Configuration
-public class JobConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory ;
-
-    public JobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
-    }
-
-    @Bean
-    public Job job() {
-        return jobBuilderFactory.get("batch01")
-            .start(step1())
-            .next(step2())
-            .next(step3())
-            .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
-            .tasklet((contribution, chunkContext) -> {
-
-                System.out.println("step1 executed");
-
-                if(1==1)
-                    throw new RuntimeException("step2 failed");
-
-
-                return RepeatStatus.FINISHED;
-            }).build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-            .tasklet((contribution, chunkContext) -> {
-
-                System.out.println("step2 executed");
-
-                if(1==1)
-                    throw new RuntimeException("step2 failed");
-
-
-                return RepeatStatus.FINISHED;
-            }).build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-            .tasklet((contribution, chunkContext) -> {
-
-                System.out.println("step2 executed");
-
-                return RepeatStatus.FINISHED;
-            }).build();
-    }
-
-    @Bean
-    public Flow flow() {
-        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<Flow>("flow");
-        return flowFlowBuilder
-                .start(step4())
-                .next(step5())
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-
-                    System.out.println("step4 executed");
-
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-
-                    System.out.println("step5 executed");
-
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package io.springbatch.jobBuilderFactory;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.job.builder.FlowBuilder;
+//import org.springframework.batch.core.job.flow.Flow;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2025-01-01        koiw1       최초 생성
+// */
+//@Configuration
+//public class JobConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory ;
+//
+//    public JobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+//        this.jobBuilderFactory = jobBuilderFactory;
+//        this.stepBuilderFactory = stepBuilderFactory;
+//    }
+//
+//    @Bean
+//    public Job job() {
+//        return jobBuilderFactory.get("batch01")
+//            .start(step1())
+//            .next(step2())
+//            .next(step3())
+//            .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
+//            .tasklet((contribution, chunkContext) -> {
+//
+//                System.out.println("step1 executed");
+//
+//                if(1==1)
+//                    throw new RuntimeException("step2 failed");
+//
+//
+//                return RepeatStatus.FINISHED;
+//            }).build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//            .tasklet((contribution, chunkContext) -> {
+//
+//                System.out.println("step2 executed");
+//
+//                if(1==1)
+//                    throw new RuntimeException("step2 failed");
+//
+//
+//                return RepeatStatus.FINISHED;
+//            }).build();
+//    }
+//
+//    @Bean
+//    public Step step3() {
+//        return stepBuilderFactory.get("step3")
+//            .tasklet((contribution, chunkContext) -> {
+//
+//                System.out.println("step2 executed");
+//
+//                return RepeatStatus.FINISHED;
+//            }).build();
+//    }
+//
+//    @Bean
+//    public Flow flow() {
+//        FlowBuilder<Flow> flowFlowBuilder = new FlowBuilder<Flow>("flow");
+//        return flowFlowBuilder
+//                .start(step4())
+//                .next(step5())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step4() {
+//        return stepBuilderFactory.get("step4")
+//                .tasklet((contribution, chunkContext) -> {
+//
+//                    System.out.println("step4 executed");
+//
+//                    return RepeatStatus.FINISHED;
+//                }).build();
+//    }
+//
+//    @Bean
+//    public Step step5() {
+//        return stepBuilderFactory.get("step5")
+//                .tasklet((contribution, chunkContext) -> {
+//
+//                    System.out.println("step5 executed");
+//
+//                    return RepeatStatus.FINISHED;
+//                }).build();
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
