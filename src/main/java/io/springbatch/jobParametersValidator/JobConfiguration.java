634df4c8f8e69aa76982b8270ac14929d0d9d0e6
koiw1202 <koiw1202@naver.com>
2025-01-19 16:47:33 +0900

    jobParametersValidator 공부

---
+++ src/main/java/io/springbatch/jobParametersValidator/JobConfiguration.java
@@ -0,0 +1,103 @@
+package io.springbatch.jobParametersValidator;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-01        koiw1       최초 생성
+ */
+@Configuration
+public class JobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory ;
+
+    public JobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batch01")
+            .start(step1())
+            .next(step2())
+            .next(step3())
+            // .validator(new CustomJobParametersValidator())
+            .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .tasklet((contribution, chunkContext) -> {
+
+                System.out.println("step1 executed");
+
+                return RepeatStatus.FINISHED;
+            }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+            .tasklet((contribution, chunkContext) -> {
+
+                System.out.println("step2 executed");
+
+                return RepeatStatus.FINISHED;
+            }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+            .tasklet((contribution, chunkContext) -> {
+
+                System.out.println("step3 executed");
+
+                return RepeatStatus.FINISHED;
+            }).build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
