06287dde27ea80f9397cc6ab9f5f48ab9efadaf9
koiw1202 <koiw1202@naver.com>
2025-01-19 22:48:21 +0900

    Job의 Incrementer 공부

--- src/main/java/io/springbatch/jobParametersValidator/CustomJobParametersValidator.java
+++ src/main/java/io/springbatch/jobParametersValidator/CustomJobParametersValidator.java
@@ -1,25 +1,25 @@
-package io.springbatch.jobParametersValidator;
-
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-01-19        koiw1       최초 생성
- */
-public class CustomJobParametersValidator implements JobParametersValidator {
-
-
-    @Override
-    public void validate(JobParameters parameters) throws JobParametersInvalidException {
-
-        if(parameters.getString("name") == null) {
-            throw new JobParametersInvalidException("name parameter is not found.");
-        }
-
-    }
-}
+//package io.springbatch.jobParametersValidator;
+//
+//import org.springframework.batch.core.JobParameters;
+//import org.springframework.batch.core.JobParametersInvalidException;
+//import org.springframework.batch.core.JobParametersValidator;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2025-01-19        koiw1       최초 생성
+// */
+//public class CustomJobParametersValidator implements JobParametersValidator {
+//
+//
+//    @Override
+//    public void validate(JobParameters parameters) throws JobParametersInvalidException {
+//
+//        if(parameters.getString("name") == null) {
+//            throw new JobParametersInvalidException("name parameter is not found.");
+//        }
+//
+//    }
+//}
